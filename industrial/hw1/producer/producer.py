0ec29e7d59d8a5eeb6ee6ecbdfcc2d075ffcc32d
svinkapeppa <erubanenko@gmail.com>
2018-02-19 13:02:12 +0300

    Add producer

---
+++ industrial/hw1/producer/producer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+import pika
+
+connection = pika.BlockingConnection(
+    pika.URLParameters("amqp://guest:guest@rabbit:5672"))
+channel = connection.channel()
+channel.queue_declare(queue='batman')
+print('[x] Connection established.')
+print('[*] Write your messages. To exit press CTRL+C.')
+try:
+    while 1:
+        data = input('[*] Enter your message: ')
+        channel.basic_publish(exchange='',
+                              routing_key='batman',
+                              body=data.encode())
+        print('[x] Sent!')
+except KeyboardInterrupt:
+    exit()
