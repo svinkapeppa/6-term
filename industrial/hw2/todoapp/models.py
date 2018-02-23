e8a4e6c7a48bfa417eff877e62e584d48aa0a5bf
svinkapeppa <erubanenko@gmail.com>
2018-02-23 18:29:26 +0300

    home page done

--- industrial/hw2/todoapp/models.py
+++ industrial/hw2/todoapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Task(models.Model):
+    """
+    Model representing single task.
+    """
+    description = models.TextField(max_length=256, help_text="Describe your task here")
+    additional_info = models.TextField(max_length=1024, help_text="Write some tips here")
+
+    TASK_STATUS = (
+        ('p', 'Pending'),
+        ('d', 'Done')
+    )
+
+    status = models.CharField(max_length=1, choices=TASK_STATUS, default='p', help_text='Set status of the task')
+
+    def __str__(self):
+        """
+        String for representing model object.
+        """
+        return self.description
+
+    def get_absolute_url(self):
+        return reverse('task-detail', args=[str(self.id)])
--- industrial/hw2/todoapp/views.py
+++ industrial/hw2/todoapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Task
+
+
+def index(request):
+    """
+    Function to display home page.
+    """
+    num_tasks = Task.objects.all().count()
+    num_done_tasks = Task.objects.filter(status__exact='d').count()
+    num_pending_tasks = Task.objects.filter(status__exact='p').count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_tasks': num_tasks,
+                 'num_done_tasks': num_done_tasks,
+                 'num_pending_tasks': num_pending_tasks},
+    )
